664f4c7a6445ed2657858465107d168d06c89347
lesinstruments <alex@lesinstruments.com>
2014-01-08 22:54:26 -0500

    code update

--- plotlyethernettemphumid/plotlyethernettemphumid.ino
+++ plotlyethernettemphumid/plotlyethernettemphumid.ino
@@ -10,13 +10,10 @@
 #define DHTPIN 2     // We have connected the DHT to Digital Pin 2
 #define DHTTYPE DHT22 // This is the type of DHT Sensor (Change it to DHT11 if you're using that model)
 DHT dht(DHTPIN, DHTTYPE); // Initialize DHT object
+plotly plotly; // initialize a plotly object, named plotly
 
-// Plotly Setup
-plotly plotly; // initialize a plotly object
-int nTraces=2; // number of traces/series of the graph
-int nPoints=1; // number of points to transmit for each trace -- all traces must transmit the same number of points
-char layout[]="{}"; // Not needed as we will be styling through the Plot.ly web GUI
-char filename[] = "Plotly Arduino API"; // name of the plot that will be saved in your plotly account -- resaving to the same filename will simply extend the existing traces with new data
+
+
 
 
 // Ethernet Setup
@@ -40,32 +37,32 @@ void setup() {
   Serial.begin(9600);
     while (!Serial) {
       ; // wait for serial port to connect. Needed for Leonardo only
-  }
-  
+  }  
   dht.begin(); // initialize dht sensor reading
-
   startEthernet();    // initialize ethernet
-
-  plotly.VERBOSE = true; // turn to false to suppress printing over serial
-  plotly.DRY_RUN = false; // turn to false when you want to connect to plotly's servers
-  plotly.username = "username"; // your plotly username -- sign up at https://plot.ly/ssu
-  plotly.api_key = "api_key"; // your plot.ly API key --> if you don't have one : plot.ly/api/key
-  plotly.timestamp = true; // Tell plotly this is a time/date graph and plotly will convert the x data automatically
-  plotly.timezone = "America/Montreal"; // Timezone -- > You can check out a full list in the Plotly-Arduino API
+  
+  
 }
 
 void loop() {
-
-
-    plotly.open_stream(nPoints, nTraces, filename, layout); // open stream to send nPoints and nTraces to plotly
-
     float h = dht.readHumidity(); // read humitidy from DHT pin
-    float t = dht.readTemperature() - 8; // read temperature from DHT pin
+    float t = dht.readTemperature() - 8; // read temperature from DHT pin (ours calibrated accuractly to our home thermostat with -8 to the temperature)
+    plotly.VERBOSE = true; // turn to false to suppress printing over serial
+    plotly.DRY_RUN = false; // turn to false when you want to connect to plotly's servers
+    plotly.username = "demos"; // your plotly username -- sign up at https://plot.ly/ssu or feel free to use this public account. password of the account is "password"
+    plotly.api_key = "tj6mr52zgp"; // "public_arduino"'s api_key -- char api_key[10]
+    plotly.timestamp = true; // tell plotly that you're stamping your data with a millisecond counter and that you want plotly to convert it into a date-formatted graph
+    plotly.timezone = "America/Montreal"; // full list of timezones is here:
+    char layout[]="{}";
+    char filename[] = "test90"; // name of the plot that will be saved in your plotly account -- resaving to the same filename will simply extend the existing traces with new data
+    Serial.println("before");
+    plotly.open_stream(1, 2, filename, layout);
 
-
-    plotly.post(millis(),h); // post humidity to plotly
-    delay(750);
-    plotly.post(millis(),t); // post temperature to plotly
-    delay(4*60000);
-
+      plotly.post(millis(),h); // post humidity to plotly
+      delay(750);
+      plotly.post(millis(),t); // post temperature to plotly
+      for(int i=0; i<10; i++){
+      delay(1000);
+      Serial.println(i+1);
+      }
   }
